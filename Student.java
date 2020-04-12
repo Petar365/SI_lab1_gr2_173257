03e07d1465eded60d12e3d3fc4285aa37a1ac13a
Petar Milcevski <petar365@gmail.com>
2020-04-12 19:06:01 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,9 +1,16 @@
 class Student {
-	String index;
-	String firstName;
-	String lastName;
+	int index;
+	char firstName;
+	char lastName;
     int grades=[];
-
+    Student(int index,char *firstName,char *lastName,int grade[]){
+    this->index=index;
+    strcpy(this->firstName, firstName);
+    this->(this->LastName, LastName);
+    for(i=0;i<5;i++){
+        this->grades[i]=grades[i];
+    }
+    }
 	//TODO constructor
 
 	//TODO seters & getters
